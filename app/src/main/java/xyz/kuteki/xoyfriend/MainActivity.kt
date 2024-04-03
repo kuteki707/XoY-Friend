6236e28b601f15335d3417b58738c12d8a14cdb0
kuteki707 <krystofedy@gmail.com>
2024-04-03 21:55:37 +0300

    Added from, to and points controls. Also added forced lightmode.

--- app/src/main/java/xyz/kuteki/xoyfriend/MainActivity.kt
+++ app/src/main/java/xyz/kuteki/xoyfriend/MainActivity.kt
@@ -1,13 +1,17 @@
 package xyz.kuteki.xoyfriend
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.github.mikephil.charting.charts.LineChart
@@ -19,9 +23,11 @@ import org.mariuszgromada.math.mxparser.Expression
 import org.mariuszgromada.math.mxparser.License
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         License.iConfirmNonCommercialUse("Edwin Kristof")
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -30,31 +36,46 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
         val lineChart = LineChart(this)
         val mainLayout = findViewById<ViewGroup>(R.id.chartContainer)
         mainLayout.addView(lineChart)
 
-        val button = findViewById<Button>(R.id.button)
-        val text_box = findViewById<EditText>(R.id.editTextText)
-        button.setOnClickListener {
-            val function = text_box.text.toString()
+        val drawButton = findViewById<Button>(R.id.drawButton)
+        val inputText = findViewById<EditText>(R.id.functionTextInput)
+        val outputText = findViewById<TextView>(R.id.outputTextView)
+        val fromText = findViewById<EditText>(R.id.fromTextInput)
+        val toText = findViewById<EditText>(R.id.toTextInput)
+        val pointsText = findViewById<EditText>(R.id.pointsTextInput)
+        drawButton.setOnClickListener {
+            val function = inputText.text.toString()
+            outputText.text = "f(x)=$function"
+            imm.hideSoftInputFromWindow(it.windowToken, 0)
 
-            lineChart.data = generateLineData(function)
+            lineChart.data = generateLineData(function,fromText.text.toString().toInt(),toText.text.toString().toInt(),pointsText.text.toString().toInt())
             lineChart.invalidate()
+
         }
 
 
 
     }
 
-    private fun generateLineData(function:String): LineData {
+    private fun generateLineData(function:String,from:Int,to:Int,points:Int): LineData {
         val entries = ArrayList<Entry>()
-        for (i in 0..9) {
+        val range_length = to - from
+        val step = range_length.toDouble() / points
+
+
+        var i = from.toDouble()
+        while(i <= to.toDouble()) {
             val e = Expression(function)
             e.addArguments(Argument("x= $i"))
 
             val y = e.calculate()
             entries.add(Entry(i.toFloat(), y.toFloat()))
+            i += step
         }
 
         val dataSet = LineDataSet(entries, "f(x)")
